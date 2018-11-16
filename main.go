e8b5579819f3b8863273d78f6bf625bfb54726a3
James Eagle <jimeagle@gmail.com>
2018-11-16 13:00:11 +0000

    Fix hmac, run scripts in yaml

--- main.go
+++ main.go
@@ -3,19 +3,21 @@ package main
 import (
 	"crypto/hmac"
 	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"strconv"
 	"strings"
 )
 
 const signaturePrefix = "sha1="
 const signatureLength = 45
 
-var events = []string{"wildcard", "check_run", "check_suite", "commit_comment", "create", "delete", "deployment", "deployment_status", "fork", "github_app_authorization", "gollum", "installation", "installation_repositories", "issue_comment", "issues", "label", "marketplace_purchase", "member", "membership", "milestone", "organization", "org_block", "page_build", "project_card", "project_column", "project", "public", "pull_request_review_comment", "pull_request_review", "pull_request", "push", "repository", "repository_import", "repository_vulnerability_alert", "release", "security_advisory", "status", "team", "team_add", "watch"}
+var events = []string{"wildcard", "check_run", "check_suite", "commit_comment", "create", "delete", "deployment", "deployment_status", "fork", "github_app_authorization", "gollum", "installation", "installation_repositories", "issue_comment", "issues", "label", "marketplace_purchase", "member", "membership", "milestone", "organization", "org_block", "page_build", "ping", "project_card", "project_column", "project", "public", "pull_request_review_comment", "pull_request_review", "pull_request", "push", "repository", "repository_import", "repository_vulnerability_alert", "release", "security_advisory", "status", "team", "team_add", "watch"}
 
 func main() {
 
@@ -23,55 +25,107 @@ func main() {
 
 		var secret = os.Getenv("WEBHOOKS_SECRET")
 		if secret == "" {
-			panic("secret not set")
+			respond500(w, "secret not found in environment")
+			return
 		}
 
+		//var delivery = r.Header.Get("X-GitHub-Delivery")
 		var signature = r.Header.Get("X-Hub-Signature")
-		var delivery = r.Header.Get("X-GitHub-Delivery")
 		var event = r.Header.Get("X-GitHub-Event")
+
+		if !sliceHasString(events, event) {
+			respond500(w, "invalid event type")
+			return
+		}
 
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
-			// todo
+			respond500(w, "can't read body")
+			return
 		}
 
-		var validSig = CheckMAC(body, []byte(signature), []byte(secret))
+		var validSig = checkMAC(body, []byte(signature), []byte(secret))
+		if !validSig {
+			respond500(w, "invalid secret")
+			return
+		}
+
+		scripts, err := readYaml()
 		if err != nil {
-			// todo
+			respond500(w, "can't read yaml: ")
+			return
 		}
 
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		for _, v := range scripts {
+			out, err := runCommand(v.Run)
+			if err != nil {
+				fmt.Println(err.Error())
+			} else {
+				fmt.Println(string(out))
+			}
+		}
+
+		respond200(w)
 	})
 
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8099", nil)
 }
 
-func CheckMAC(message, messageMAC, key []byte) bool {
+func respond500(w http.ResponseWriter, message string) {
+	fmt.Println(strconv.Itoa(500) + message)
+	w.WriteHeader(500)
+	w.Write([]byte(message))
+}
 
-	if len(messageMAC) != signatureLength || !strings.HasPrefix(messageMAC, signaturePrefix) {
+func respond200(w http.ResponseWriter) {
+	fmt.Println(200)
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+func runCommand(cmd string) (out []byte, err error) {
+
+	fmt.Println("Executing: " + cmd)
+	//return
+	return exec.Command("sh", "-c", cmd).Output()
+}
+
+func checkMAC(message, messageMAC, key []byte) bool {
+
+	if len(messageMAC) != signatureLength || !strings.HasPrefix(string(messageMAC), signaturePrefix) {
 		return false
 	}
 
 	mac := hmac.New(sha1.New, key)
 	mac.Write(message)
-	expectedMAC := mac.Sum(nil)
-	return hmac.Equal(messageMAC, expectedMAC)
+	expectedMAC := hex.EncodeToString(mac.Sum(nil))
+
+	return hmac.Equal([]byte(signaturePrefix+expectedMAC), messageMAC)
 }
 
-func readYaml() (ret []YamlScript) {
+func readYaml() (ret []YamlScript, err error) {
 
 	yamlFile, err := ioutil.ReadFile("scripts/scripts.yaml")
 	if err != nil {
-		log.Printf("yamlFile.Get err   #%v ", err)
+		return ret, err
 	}
 
-	err = yaml.Unmarshal(yamlFile, ret)
+	err = yaml.Unmarshal(yamlFile, &ret)
 	if err != nil {
-		log.Fatalf("Unmarshal: %v", err)
+		return ret, err
 	}
 
-	return ret
+	return ret, err
 
+}
+
+func sliceHasString(slice []string, i string) bool {
+	for _, v := range slice {
+		if v == i {
+			return true
+		}
+	}
+	return false
 }
 
 type YamlScript struct {
